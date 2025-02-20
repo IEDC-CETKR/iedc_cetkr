dd174a43b576ee75be6dbc65608f79d8eebd4a59
siyad01 <144800182+siyad01@users.noreply.github.com>
2025-02-20 06:23:48 +0530

    Prepare for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+      plugins: [tailwindcss()],
+      ssr: {
+        external: ['@fortawesome/fontawesome-free'],
+      },
+    },
+  });
